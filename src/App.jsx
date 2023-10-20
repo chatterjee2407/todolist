0f005143f1571f45147bd88d1f56d98b6bfd84a2
viperist <somnathc276@gmail.com>
2023-10-20 11:40:03 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
-import "./styles.css"
+ import {useEffect , useState } from "react"
+ import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
+import { Todolist } from "./Todolist"
 
 
 
 export default function App(){
+   const [todos,setTodos]=useState(() => {
+        const localValue = localStorage.getItem("ITEMS")
+        if(localValue == null) return []
+        
+        return JSON.parse(localValue)
+   })
+
+   useEffect(()=>{
+    localStorage.setItem("ITEMS",JSON.stringify(todos))
+   },[todos])
+
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return[...currentTodos ,
+        {id: crypto.randomUUID(),title,completed:false},
+      ]
+    })
+
+  }
+
+  
+  function toggleTodo(id,completed) {
+      setTodos(currentTodos => {
+        return currentTodos.map(todo => {
+          if(todo.id === id){
+            return {...todo,completed}
+          }
+
+          return todo
+        })
+      })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos =>{
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return(
-   <form className="new-item-form">
-    <div className="form-row">
-    <label htmlFor="item">New Item</label>
-    <input type="text" id = "item" />
-    </div>
-  </form>
+    <>
+   <NewTodoForm onSubmit={addTodo}/>
+  <h1 className="header">ToDo List</h1>
+      <Todolist todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+  </>
   )
 }
